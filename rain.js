ebbbdac586d6848188f3d05b3a0a4c9c8eab5008
Windyx <windyxeditor@gmail.com>
2026-09-02 21:35:07 +0300

    Add files via upload

--- rain.js
+++ rain.js
@@ -22547,7 +22547,7 @@ render().catch(error => {
       };
       RainNSFWBypass_default = definePlugin({
         name: "RainNSFWBypass",
-        description: "\u041F\u043E\u043B\u043D\u044B\u0439 \u043E\u0431\u0445\u043E\u0434 NSFW: \u0440\u0430\u0437\u0431\u043B\u043E\u043A\u0438\u0440\u043E\u0432\u043A\u0430 \u043A\u0430\u043D\u0430\u043B\u043E\u0432 \u0438 \u0444\u043E\u0442\u043E (Yoti Bypass).",
+        description: "\u041F\u043E\u043B\u043D\u044B\u0439 \u043E\u0431\u0445\u043E\u0434 NSFW (Fix \u043A\u0435\u0448\u0430 \u043F\u0440\u0438 \u0441\u0442\u0430\u0440\u0442\u0435).",
         author: [
           {
             name: "WindyxCXX",
@@ -22555,7 +22555,7 @@ render().catch(error => {
           }
         ],
         id: "RainNSFWBypass",
-        version: "4.0.0",
+        version: "4.1.0",
         start() {
           var UserStore3 = findByProps("getCurrentUser", "getUser");
           if (UserStore3) {
@@ -22581,6 +22581,25 @@ render().catch(error => {
               type: "CURRENT_USER_UPDATE",
               user: UserStore3?.getCurrentUser()
             });
+            var SelectedChannelStore2 = findByProps("getChannelId", "getVoiceChannelId");
+            if (SelectedChannelStore2 && ChannelStore3) {
+              var currentChannelId = SelectedChannelStore2.getChannelId();
+              if (currentChannelId) {
+                var currentChannel = ChannelStore3.getChannel(currentChannelId);
+                if (currentChannel) {
+                  overrideChannel(currentChannel);
+                  try {
+                    FluxDispatcher2.dispatch({
+                      type: "CHANNEL_UPDATES",
+                      channels: [
+                        currentChannel
+                      ]
+                    });
+                  } catch (e) {
+                  }
+                }
+              }
+            }
           }
         },
         stop() {
